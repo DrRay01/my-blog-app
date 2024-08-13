42a52c79d32d575381f6f9062f4cb7a6e782fe4a
ATTAH RAYMOND KWAME <90375276+DrRay01@users.noreply.github.com>
2024-08-13 18:50:50 +0000

    update on my blog-app

---
+++ src/Home.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import BlogList from "./BlogList";
+
+const Home = () => {
+  const [blogs, setBlogs] = useState(null);
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
+
+  return (
+    <div className="home">
+      {/* <BlogList
+        blogs={blogs}
+        title="All the Blogs"
+        handleDelete={handleDelete}
+      /> */}
+    </div>
+  );
+};
+
+export default Home;
