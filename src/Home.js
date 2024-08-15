374ea41276657f3d9a453e6d676bd2fee4d2864a
ATTAH RAYMOND KWAME <90375276+DrRay01@users.noreply.github.com>
2024-08-15 18:16:01 +0000

    my changes

--- src/Home.js
+++ src/Home.js
@@ -1,31 +1,17 @@
 import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
-
+import useFetch from "./useFetch";
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
-
-  useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-      });
-  }, []);
-
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
   return (
     <div className="home">
-      {/* <BlogList
-        blogs={blogs}
-        title="All the Blogs"
-        handleDelete={handleDelete}
-      /> */}
+      {error && <div>{error}</div>}
+      {isPending && <div>Data is Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All the Blogs" />}
     </div>
   );
 };
